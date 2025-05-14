7a71afb92df3785f7067571f571e042414cc20df
crownclown501 <55864043+crownclown501@users.noreply.github.com>
2025-05-14 00:17:34 -0600

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php
+    require '../includes/funciones.php';
+    incluirTemplate('header');
+?>
+<main class="contenedor">
+    <h1>Administrador de Bienes Raices</h1>
+    <a href="/admin/propiedades/crear.php" class="boton btn-verde">Nueva Propiedad</a>
+</main>
+<?php incluirTemplate('footer'); ?>
