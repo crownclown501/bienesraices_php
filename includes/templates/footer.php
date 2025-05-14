a4f862cac2b2ddcd7c7bd78a35a4b0afb942715d
crownclown501 <55864043+crownclown501@users.noreply.github.com>
2025-05-14 00:18:15 -0600

    Add files via upload

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -10,7 +10,7 @@
     </div>
     <p class="copyright">Todos los derechos Reservados <?php echo date('M-Y')?> &copy;</p>
 </footer>
-<script src="build/js/bundle.min.js"></script>
+<script src="/build/js/bundle.min.js"></script>
 </body>
 
 </html>
